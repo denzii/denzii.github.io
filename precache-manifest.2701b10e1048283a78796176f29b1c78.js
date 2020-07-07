665cf77d2ddec5265ac696960b4f4d9d1ccbd629
denizarca <denizarca1@gmail.com>
2020-07-08 00:51:02 +0300

    Updates

---
+++ precache-manifest.2701b10e1048283a78796176f29b1c78.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "558da7d3bddd2dda698d40fddfa7856d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93ea63e7d04a063754ab",
+    "url": "/static/css/main.d665fb0f.chunk.css"
+  },
+  {
+    "revision": "de9d4643aeb8577b54fe",
+    "url": "/static/js/2.6f311e0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6f311e0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93ea63e7d04a063754ab",
+    "url": "/static/js/main.199fda76.chunk.js"
+  },
+  {
+    "revision": "f53ad8bb13a47484d900",
+    "url": "/static/js/runtime-main.9fd18a46.js"
+  },
+  {
+    "revision": "ce4d18f2754c44e7f873f4b4ae532d98",
+    "url": "/static/media/Deniz_Arca_Resume.ce4d18f2.PDF"
+  },
+  {
+    "revision": "bb80259218474922da4b227e95180bab",
+    "url": "/static/media/deniz.bb802592.png"
+  },
+  {
+    "revision": "26b5d4455da6dd153ee03028178db14b",
+    "url": "/static/media/hike2.26b5d445.png"
+  },
+  {
+    "revision": "1e4580e8993c5ce20c5f1982f97c38ea",
+    "url": "/static/media/hull3.1e4580e8.png"
+  },
+  {
+    "revision": "4fdf2092c04e6abae7b3f8036cd5f56e",
+    "url": "/static/media/kilise.4fdf2092.jpg"
+  },
+  {
+    "revision": "e729c8d62509be62d0d63030071085b1",
+    "url": "/static/media/mandala.e729c8d6.png"
+  },
+  {
+    "revision": "8c088b6cc989264c0f4d31a2669e4a19",
+    "url": "/static/media/project.8c088b6c.png"
+  },
+  {
+    "revision": "8e144a2993cb14e7d0f9c9583660077d",
+    "url": "/static/media/wren.8e144a29.png"
+  }
+]);
