43334e069463803cb010776cbd5d095666800993
Arca <denizarca1@gmail.com>
2020-07-07 18:18:12 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { StrictMode, Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//could also import without the .js extension
+import Navigation from './Navigation.js';
+import Header from './Header.js';
+import Social from './Social.js';
+import Supplementary from './Supplementary.js';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navigation Header="Deniz Arca" />
+        <Header />
+        <Social />
+        <Supplementary />
+      </div>
+    );
+  }
+}
+
+//below line connects the app with index.html
+//can specify an imported class by doing <ClassName />
+//any usage in between <React.StrictMode> tags will include additional checks to help with dev
+ReactDOM.render(<StrictMode> <App /> </StrictMode>, document.getElementById('root'));
+
+
 serviceWorker.unregister();
